db12c6ef322fa5592c31c93edaa3a89261316f79
Haerunnas <nassjourney@gmail.com>
2026-04-07 07:29:14 +0700

    Fix:  Routing Validation

--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,44 @@
-import { type NextRequest } from 'next/server'
+import { type NextRequest, NextResponse } from 'next/server'
 import { updateSession } from '@/lib/supabase/proxy'
+import { createClient } from './lib/supabase/server'
 
 export async function proxy(request: NextRequest) {
-  return await updateSession(request)
+
+  const response = await updateSession(request)
+  const supabase = await createClient()
+  const { data: { user } } = await supabase.auth.getUser()
+  const url = request.nextUrl.clone()
+
+  if (url.pathname.startsWith('/admin')) {
+    // Jika tidak login, lempar ke login
+    if (!user) {
+      return NextResponse.redirect(new URL('/login', request.url))
+    }
+
+    // Ambil role dari tabel profiles
+    const { data: profile } = await supabase
+      .from('profiles')
+      .select('role')
+      .eq('id', user.id)
+      .single()
+
+    if (profile?.role !== 'ADMIN') {
+      return NextResponse.redirect(new URL('/user/dashboard', request.url))
+    }
+  }
+
+  if (url.pathname.startsWith('/user')) {
+    if (!user) {
+      return NextResponse.redirect(new URL('/login', request.url))
+    }
+  }
+
+  if ((url.pathname === '/login' || url.pathname === '/register') && user) {
+    return NextResponse.redirect(new URL('/user/dashboard', request.url))
+  }
+
+  return response
+  // return await updateSession(request)
 }
 export const config = {
   matcher: [
