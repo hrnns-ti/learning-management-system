108878d97fb258d52bd14e5d780c0911f7e658a1
Haerunnas <nassjourney@gmail.com>
2026-04-12 13:01:50 +0700

    Fix: middleware

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        'localhost:3000', 
+        'learning-management-system-exw.pages.dev'
+      ],
+    },
+  },
 };
 
 export default nextConfig;
